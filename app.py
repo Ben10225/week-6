fca3bd6d29c0bbfb1eab190c118acd316f8ed857
pon <pon@ponde-MacBook-Pro.local>
2022-10-26 19:21:53 +0800

    use connect pool

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from email import message
 from flask import Flask, render_template, redirect, request, session
 
 import mysql.connector
@@ -9,14 +8,28 @@ app.debug = True
 
 app.secret_key="anytxt"
 
-mydb = mysql.connector.connect(
-  host="localhost",    
-  user="root",  
-  passwd=sqlPwd(),  
-  database="week6"
-)
+# mydb = mysql.connector.connect(
+#   host="localhost",    
+#   user="root",  
+#   passwd=sqlPwd(),  
+#   database="week6"
+# )
 
-mycursor = mydb.cursor()
+from mysql.connector import Error
+from mysql.connector import pooling
+
+def connectPool():
+  connection_pool = pooling.MySQLConnectionPool(pool_name="pynative_pool",
+                                                pool_size=5,
+                                                pool_reset_session=True,
+                                                host='localhost',
+                                                user='root',
+                                                database='week6',
+                                                password=sqlPwd())
+  connection_object = connection_pool.get_connection()   
+  return connection_object                                          
+
+# mycursor = connection_pool.cursor()
 insertSql = "INSERT INTO users (name, username, password) VALUES (%s, %s, %s)"
 signUpSelectSql = "SELECT * FROM users WHERE username=%s"
 msgSql = "INSERT INTO message (name_id, msg) VALUES (%s, %s)"
@@ -37,9 +50,15 @@ def member():
   name = session["name"]
   if not name:
     return redirect("/")
-  
+
+  db = connectPool()
+  mycursor = db.cursor()
   mycursor.execute(showMsgSql)
   msgs = mycursor.fetchall()
+  mycursor.close()
+  db.close()
+
+  # msgs = connectDB(None, None, False, True, False, False)
   time = []
   for i in msgs:
     s = str(i[2]).split(" ")[1][:5]
@@ -62,14 +81,24 @@ def signup():
   password = request.json['password']
   if name == "" or username == "" or password == "":
     return {"result": "請輸入註冊資訊"}
+
+  db = connectPool()
+  mycursor = db.cursor()
   mycursor.execute(signUpSelectSql, (name, ))
   exists = mycursor.fetchone()
+
+  # exists = connectDB(signUpSelectSql, (name, ), True, False, False, False)
   if exists:
+    mycursor.close()
+    db.close()
     return {"result": "帳號已存在"}
   else:
     val = (name, username, password)
     mycursor.execute(insertSql, val)
-    mydb.commit()
+    db.commit()
+    mycursor.close()
+    db.close()
+    # connectDB(insertSql, val, False, False, True, False)
     print("用戶 {} 帳戶創建成功！".format(name))
     return {"result": "OK"}
 
@@ -82,13 +111,24 @@ def sign():
   if username == "" or password == "":
     return {"result": "請輸入登入資訊"}
   params = (username, password)
+
+  # exists = connectDB(signInSelectSql, params, True, False, False, False)
+
+  db = connectPool()
+  mycursor = db.cursor(dictionary=True) 
+  # db.cursor(dictionary=True) 資料 return 會變字典
+
   mycursor.execute(signInSelectSql, params)
   exists = mycursor.fetchone()
+  mycursor.close()
+  db.close()
+
   if not exists:
     return {"result": "帳號或密碼輸入錯誤"}
   
-  session["name"] = exists[1]
+  session["name"] = exists["name"]
   return {"result": "OK"}
+  
 
 
 # 登出
@@ -100,25 +140,39 @@ def signout():
 
 # 留言
 @app.route("/message", methods=["post"])
-def stayMsg():
-  name = session["name"]
-  mycursor.execute(msgUidSelectSql, (name, ))
-  uid = mycursor.fetchone()[0]
-
+def message():
   msg = request.json["msg"]
   clicked = request.json["click"]
   if not clicked:
+    db = connectPool()
+    mycursor = db.cursor()
     mycursor.execute(showMsgSql)
     msgs = mycursor.fetchall()
+    mycursor.close()
+    db.close()
+    # msgs = connectDB(None, None, False, True, False, False)
     return {"result": msgs}
   if not msg:
     return {"result": "請留言"}
-  valMsg = (uid, msg)
-  mycursor.execute(msgSql, valMsg)
-  mydb.commit()
-
+  
+  name = session["name"]
+  
+  db = connectPool()
+  mycursor = db.cursor()
+  # 紀錄使用者及留言
+  mycursor.execute(msgUidSelectSql, (name, ))
+  uid = mycursor.fetchone()[0]
+  val = (uid, msg)
+  # 寫入
+  mycursor.execute(msgSql, val)
+  db.commit()
+  # 抓所有留言
   mycursor.execute(showMsgSql)
   msgs = mycursor.fetchall()
+  mycursor.close()
+  db.close()
+  # msgs = connectDB(msgUidSelectSql, (name, msg), False, False, False, True)
+
   # for i in msgs:
   #   print(i[0], i[1], i[2])
   return {"result": msgs}
@@ -126,3 +180,65 @@ def stayMsg():
 
 app.run(port=3000)
 
+
+
+
+
+
+
+
+
+
+
+
+# def connectDB(item1, item2, fetchonebool, memberbool, signupbool, msgbool):
+#   try:
+#     connection_pool = pooling.MySQLConnectionPool(pool_name="pynative_pool",
+#                                                   pool_size=5,
+#                                                   pool_reset_session=True,
+#                                                   host='localhost',
+#                                                   user='root',
+#                                                   database='week6',
+#                                                   password=sqlPwd())
+
+#     # print("Printing connection pool properties ")
+#     # print("Connection Pool Name - ", connection_pool.pool_name)
+#     # print("Connection Pool Size - ", connection_pool.pool_size)
+
+#     # Get connection object from a pool
+#     connection_object = connection_pool.get_connection()
+
+#     if connection_object.is_connected():
+
+#       mycursor = connection_object.cursor()
+#       if fetchonebool:
+#         mycursor.execute(item1, item2)
+#         exists = mycursor.fetchone()
+#         return exists
+
+#       if memberbool:
+#         mycursor.execute(showMsgSql)
+#         msgs = mycursor.fetchall()
+#         return msgs
+        
+#       if signupbool:
+#         mycursor.execute(item1, item2)
+#         connection_object.commit()
+
+#       if msgbool:
+#         mycursor.execute(item1, (item2[0], ))
+#         uid = mycursor.fetchone()[0]
+#         mycursor.execute(msgSql, (uid, item2[1]))
+#         connection_object.commit()
+
+#         mycursor.execute(showMsgSql)
+#         msgs = mycursor.fetchall()
+#         return msgs
+
+#   except Error as e:
+#     print("Error while connecting to MySQL using Connection pool ", e)
+#   finally:
+#     if connection_object.is_connected():
+#       mycursor.close()
+#       connection_object.close()
+#       print("MySQL connection is closed")
